41dae17b173539aa5889d33eec383a7fa9c9b6d0
Leo Cavalcante <lc@leocavalcante.com>
2012-07-30 08:32:51 -0400

    removing kafifa

--- puzzle.js
+++ puzzle.js
@@ -1,10 +1,7 @@
-(function ( puzzle, undefined ) {
-    puzzle = {
+module.exports = (function () {
+    return {
         fizzbuzz: function ( n ) {
             return ( ( n % 3 ) ? '' : 'Fizz' ) + ( ( n % 5 ) ? '' : 'Buzz' ) || n;
-        },
-        kafifa: function ( n ) {
-            return ( ( n % 7 ) && ( ( n - 7 ) % 10 ) ) ? n : 'Kafifa';
         }
     };
-}( typeof window === 'undefined' ? module.exports : window.puzzle ));
+}());
--- test/node.js
+++ test/node.js
@@ -2,11 +2,30 @@ var puzzle = require( '../puzzle' ),
     vows   = require( 'vows' ),
     assert = require( 'assert' );
 
+// testing fizzbuzz
 vows.describe( 'fizzbuzz' ).addBatch({
     'when the number is divisible by 3': {
         topic: puzzle.fizzbuzz( 3 ),
         'is Fizz': function ( topic ) {
             assert.equal( topic, 'Fizz' );
+        }
+    },
+    'when the number is divisible by 5': {
+        topic: puzzle.fizzbuzz( 5 ),
+        'is Buzz': function ( topic ) {
+            assert.equal( topic, 'Buzz' );
+        }
+    },
+    'when the number is divisible by 3 and 5': {
+        topic: puzzle.fizzbuzz( 15 ),
+        'is FizzBuzz': function ( topic ) {
+            assert.equal( topic, 'FizzBuzz' );
+        }
+    },
+    'when the number is NOT divisible by 3 OR 5': {
+        topic: puzzle.fizzbuzz( 2 ),
+        'is it self': function ( topic ) {
+            assert.equal( topic, 2 );
         }
     }
 }).run();
