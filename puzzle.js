8f439a002b55586ff56f205628b9938ae7f674a2
Leo Cavalcante <lc@leocavalcante.com>
2012-07-30 07:46:58 -0400

    added puzzle module file

---
+++ puzzle.js
@@ -0,0 +1,8 @@
+(function ( puzzle, undefined ) {
+    puzzle = {
+        fizzBuzz: function ( n ) {
+        },
+        kafifa: function ( n ) {
+        }
+    };
+}( typeof window === 'undefined' ? module.exports : window.puzzle ));
