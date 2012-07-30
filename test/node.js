0ab60aefe6cdcbd3cd5032c8c6c62516fd271d82
Leo Cavalcante <lc@leocavalcante.com>
2012-07-30 08:01:15 -0400

    renamed test/test.js to test/node.js

---
+++ test/node.js
@@ -0,0 +1,11 @@
+var puzzle = require( './puzzle' ),
+    assert = require( 'assert' );
+
+describe( 'puzzle', function () {
+    describe( 'fizzbuzz', function () {
+        it( 'should return Fizz for divisible by 3', function () {
+            assert.equal( 'Fizz', puzzle.fizzbuzz( 3 ) );
+            assert.equal( 'Fizz', puzzle.fizzbuzz( 9 ) );
+        });
+    });
+});
--- test/test.js
+++
@@ -1,3 +0,0 @@
-var puzzle = require( './puzzle' ),
-    assert = require( 'assert' );
-
