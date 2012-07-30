a4e6af0250c1f0b43770523e1eeb8e32558170ed
Leo Cavalcante <lc@leocavalcante.com>
2012-07-30 10:24:05 -0400

    refactored module

--- puzzle.js
+++ puzzle.js
@@ -1,7 +1,18 @@
-module.exports = (function () {
-    return {
-        fizzbuzz: function ( n ) {
-            return ( ( n % 3 ) ? '' : 'Fizz' ) + ( ( n % 5 ) ? '' : 'Buzz' ) || n;
+(function ( global, undefined ) {
+    "use strict";
+    
+    var puzzle = {
+        fizzbuzz: function ( i, j ) {
+            if ( typeof j === 'undefined' ) {
+                return ( ( i % 3 ) ? '' : 'Fizz' ) + ( ( i % 5 ) ? '' : 'Buzz' ) || i;
+            } else {                
+                var fb = [];
+                for ( ; i <= j; i++ )
+                    fb.push( puzzle.fizzbuzz( i ) );
+                return fb;
+            }
         }
     };
-}());
+    
+    global.exports ? global.exports = puzzle : global.puzzle = puzzle;
+}( typeof window !== 'undefined' ? window : module ));
