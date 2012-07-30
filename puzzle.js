6e227bbf7a573871c482d6a38bbb3bdf8b6d296d
Leo Cavalcante <lc@leocavalcante.com>
2012-07-30 07:48:48 -0400

    added puzzles implementation

--- puzzle.js
+++ puzzle.js
@@ -1,8 +1,10 @@
 (function ( puzzle, undefined ) {
     puzzle = {
         fizzBuzz: function ( n ) {
+            return ( ( n % 3 ) ? '' : 'Fizz' ) + ( ( n % 5 ) ? '' : 'Buzz' ) || n;
         },
         kafifa: function ( n ) {
+            return ( ( n % 7 ) && ( ( n - 7 ) % 10 ) ) ? n : 'Kafifa';
         }
     };
 }( typeof window === 'undefined' ? module.exports : window.puzzle ));
